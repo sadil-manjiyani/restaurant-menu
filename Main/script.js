4dd8fae8b35a3581160e015f46802a4b423d30d6
SADIL MANJIYANI <manjiyanisadil88@gmail.com>
2021-06-07 16:06:25 +0530

    Cart added

--- Main/script.js
+++ Main/script.js
@@ -47,7 +47,8 @@ for (var i = 0; i < decrementButton.length; i++) {
     })
 }
 
-
+// cart 
+var itemName, quantity, price;
 //Displaying counter and hidding add button
 for (var i = 0; i < addButton.length; i++) {
     var button = addButton[i];
@@ -61,5 +62,61 @@ for (var i = 0; i < addButton.length; i++) {
         input = counterSelected.children[1];
         input.value = 1;
 
+        // cart
+        parentCard = buttonClicked.parentElement
+        itemName = parentCard.children[0].innerText;
+        price = parentCard.children[1].innerText;
+        quantity = input.value;
+        console.log(itemName)
+        console.log(price)
+        console.log("Quantity=" + quantity)
+        appendToCart();
     })
 }
+var customBtn = document.getElementsByClassName('customBtn');
+for (var i = 0; i < customBtn.length; i++) {
+    var incDecButton = customBtn[i];
+    incDecButton.addEventListener('click', (event) => {
+        quantity = input.value;
+        console.log("Quantity=" + quantity)
+
+    })
+
+}
+
+//Appending new items in cart
+
+var table = document.getElementById('table');
+var count = 0;
+function appendToCart() {
+    // var tr = document.createElement("tr");
+    // var listItem = document.createElement(`<tr><th scope='col'>1</th> 
+    // <th scope='col'>${itemName}</th>
+    // <th scope='col'>${quantity}</th>
+    // <th scope='col'>${price}</th>
+    // <th scope='col'>Remove</th></tr>`);
+    // table.append(listItem);
+    ++count
+    var row = table.insertRow(-1);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell5 = row.insertCell(4);
+    cell1.innerHTML = count;
+    cell2.innerHTML = itemName;
+    cell3.innerHTML = quantity;
+    cell4.innerHTML = price;
+    cell5.innerHTML = "<button class='removeButton' >" + "remove" + "</button>"
+
+
+
+}
+
+// // Deleting row using sr number Sr number available in count
+// function removeRow(count) {
+//     table.deleteRow(count);
+// }
+
+
